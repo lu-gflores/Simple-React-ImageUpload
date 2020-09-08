2b40174ef5bb4736d03205c81013d8d2ccfff19c
George Flores <georgeflores886@gmail.com>
2020-09-08 17:25:47 -0400

    added states for storing and passing files

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
+  //stores file in state and uploads when Upload button 
+  //is clicked
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    }
+  }
+  //pass file to state
   onChangeHandler = event => {
-    console.log(event.target.files[0])
+    this.setState({selectedFile: event.target.files[0],
+      loaded: 0,})
   }
 
   render() {
@@ -15,7 +24,7 @@ class App extends Component {
             <form method="post" action="#" id="#">
               <div className="form-group files">
                 <label>Upload Your File </label>
-                <input type="file" className="form-control" onChange={this.onChange}/>
+                <input type="file" className="form-control" onChange={this.onChangeHandler}/>
                 <button type="button" className='btn btn-success btn-lg btn-block'>Upload</button>
               </div>
             </form>
